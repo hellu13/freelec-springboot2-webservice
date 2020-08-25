95b454aed0a589f94bd098ecce163d342672b515
kyungmo <hellu13@naver.com>
2020-08-26 08:48:14 +0900

    전체 조회 화면 만들기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
-// Posts 클래스로 Database 를 접근하게 해 줄 JpaRepository
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+// Posts 클래스로 Database 를 접근하게 해 줄 JpaRepository
 public interface PostsRepository extends JpaRepository<Posts, Long>{  // DB Layer 접근자, <Entity 클래스, PK 타입>, CRUD 메소드가 자동으로 생성
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,12 +1,24 @@
 package com.jojoldu.book.springboot.web;
+import com.jojoldu.book.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
-public class IndexController {
+public class IndexController {  // 페이지에 관련된 컨트롤러
+
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
