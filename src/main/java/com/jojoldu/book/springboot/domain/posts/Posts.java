b0a46f08da77e567ab38cc91af82e98f61efeb5a
kyungmo <hellu13@naver.com>
2020-08-26 06:42:48 +0900

    머스테치로 화면 구성

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -10,10 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter  // 클래스 내 모든 필드의 Getter 메소드를 자동생성
-@NoArgsConstructor  // 기본 생성자 자동 추가
-@Entity  // 테이블과 링크될 클래스임을 나타냄
-public class Posts extends BaseTimeEntity {
+@Getter  // lombok 어노테이션, 클래스 내 모든 필드의 Getter 메소드를 자동생성
+@NoArgsConstructor  // lombok 어노테이션, 기본 생성자 자동 추가
+@Entity  // JPA 어노테이션, 테이블과 링크될 클래스임을 나타냄
+public class Posts extends BaseTimeEntity {  // 실제 DB 의 테이블과 매칭될 클래스( Entity 클래스 )
     @Id  // 해당 테이블의 PK 필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // PK의 생성 규칙을 나타냄, 옵션 = auto_increment
     private Long id;
@@ -26,7 +26,7 @@ public class Posts extends BaseTimeEntity {
 
     private String author;
 
-    @Builder
+    @Builder  // 빌더 패턴 클래스 생성, 생성자에 포함된 필드만 빌더에 포함
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+// Posts 클래스로 Database 를 접근하게 해 줄 JpaRepository
 
-public interface PostsRepository extends JpaRepository<Posts, Long>{  // DB Layer 접근자, <Entity 클래스, PK 타입>
+public interface PostsRepository extends JpaRepository<Posts, Long>{  // DB Layer 접근자, <Entity 클래스, PK 타입>, CRUD 메소드가 자동으로 생성
 
 }
